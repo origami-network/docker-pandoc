9bcf8473384302aa95f13433f9f2e6b1fbc14287
Bart Dubois <dubcio@o2.pl>
2018-10-05 00:39:16 +0200

    Implement generating HTML acceptance criteria

--- Spec/Pandoc.Tests.ps1
+++ Spec/Pandoc.Tests.ps1
@@ -13,9 +13,44 @@ Import-Module (Join-Path $ModulesPath 'TestDrive.psm1') -Force
 
 Describe "Pandoc image" {
     Context "from markdown" {
+        $input = New-TestDriveFile 'input.markdown' @"
+Hello *pandoc*!
+
+- one
+- two
+"@
+
+        $volumePath = "C:\Volume"
+        $volumeFile = Join-Path $volumePath $input.File
+        $docker = @(
+            'run',
+            '-v', "$($TestDrive):$($volumePath)"
+            $ImageName
+        )
 
         It "generates HTML" {
-            Write-Error "TODO: implement test case"
+            $resultFileName = 'output.html'
+            $pandoc = @(
+                '-f', 'markdown'
+                '-t', 'html',
+                $volumeFile,
+                '-o', (Join-Path $volumePath $resultFileName)
+            )
+            $arguments = $docker + $pandoc
+
+            Write-Host "> docker $($arguments -join ' ')"
+            & docker $arguments |
+                Write-Host
+
+            $LASTEXITCODE |
+                Should -Be 0
+            $resultFile = Join-Path $TestDrive $resultFileName
+            $resultFile |
+                Should -Exist
+            $resultFile |
+                Should -FileContentMatchExactly '<p>Hello <em>pandoc</em>!</p>' 
+            $resultFile |
+                Should -FileContentMatchExactly '<li>two</li>' 
         }
 
         It "generates standalone HTML" {
