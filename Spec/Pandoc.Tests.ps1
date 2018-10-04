5729367baa026dddf05b042f6ce78726bb9d2111
Bart Dubois <dubcio@o2.pl>
2018-10-05 01:08:18 +0200

    Implement all acceptance criteria for markdown
     * standeleone HTML
     * Word
     * ePUB

--- Spec/Pandoc.Tests.ps1
+++ Spec/Pandoc.Tests.ps1
@@ -12,28 +12,29 @@ Import-Module (Join-Path $ModulesPath 'TestDrive.psm1') -Force
 
 
 Describe "Pandoc image" {
+
     Context "from markdown" {
-        $input = New-TestDriveFile 'input.markdown' @"
-Hello *pandoc*!
-
-- one
-- two
-"@
-
         $volumePath = "C:\Volume"
-        $volumeFile = Join-Path $volumePath $input.File
         $docker = @(
             'run',
             '-v', "$($TestDrive):$($volumePath)"
             $ImageName
         )
 
+        $input = New-TestDriveFile 'input.markdown' @"
+Hello *pandoc*!
+
+- one
+- two
+"@
+        $volumeInputPath = (Join-Path $volumePath $input.File)
+
         It "generates HTML" {
             $resultFileName = 'output.html'
             $pandoc = @(
                 '-f', 'markdown'
                 '-t', 'html',
-                $volumeFile,
+                $volumeInputPath,
                 '-o', (Join-Path $volumePath $resultFileName)
             )
             $arguments = $docker + $pandoc
@@ -53,16 +54,84 @@ Hello *pandoc*!
                 Should -FileContentMatchExactly '<li>two</li>' 
         }
 
+        $metadata = New-TestDriveFile 'metadata.txt' @"
+---
+title: Metadata Title
+author: Metadata Author
+...
+"@
+        $volumeMetadataFile = (Join-Path $volumePath $metadata.File)
+
         It "generates standalone HTML" {
-            Write-Error "TODO: implement test case"
+            $resultFileName = 'output.html'
+            $pandoc = @(
+                '-f', 'markdown'
+                '-t', 'html',
+                '-s',
+                $volumeMetadataFile, $volumeInputPath,
+                '-o', (Join-Path $volumePath $resultFileName)
+            )
+            $arguments = $docker + $pandoc
+
+            Write-Host "> docker $($arguments -join ' ')"
+            & docker $arguments |
+                Write-Host
+
+            $LASTEXITCODE |
+                Should -Be 0
+            $resultFile = Join-Path $TestDrive $resultFileName
+            $resultFile |
+                Should -Exist
+            $resultFile |
+                Should -FileContentMatchExactly '<title>Metadata Title</title>' 
+            $resultFile |
+                Should -FileContentMatchExactly '<meta name="author" content="Metadata Author" />' 
+            $resultFile |
+                Should -FileContentMatchExactly '<p>Hello <em>pandoc</em>!</p>' 
+            $resultFile |
+                Should -FileContentMatchExactly '<li>two</li>'
         }
 
         It "generates Word" {
-            Write-Error "TODO: implement test case"
+            $resultFileName = 'output.docx'
+            $pandoc = @(
+                '-f', 'markdown'
+                '-t', 'docx',
+                $volumeMetadataFile, $volumeInputPath,
+                '-o', (Join-Path $volumePath $resultFileName)
+            )
+            $arguments = $docker + $pandoc
+
+            Write-Host "> docker $($arguments -join ' ')"
+            & docker $arguments |
+                Write-Host
+
+            $LASTEXITCODE |
+                Should -Be 0
+            $resultFile = Join-Path $TestDrive $resultFileName
+            $resultFile |
+                Should -Exist
         }
 
         It "generates ePUB" {
-            Write-Error "TODO: implement test case"
+            $resultFileName = 'output.epub'
+            $pandoc = @(
+                '-f', 'markdown'
+                '-t', 'epub',
+                $volumeMetadataFile, $volumeInputPath,
+                '-o', (Join-Path $volumePath $resultFileName)
+            )
+            $arguments = $docker + $pandoc
+
+            Write-Host "> docker $($arguments -join ' ')"
+            & docker $arguments |
+                Write-Host
+
+            $LASTEXITCODE |
+                Should -Be 0
+            $resultFile = Join-Path $TestDrive $resultFileName
+            $resultFile |
+                Should -Exist
         }
     }
 }
